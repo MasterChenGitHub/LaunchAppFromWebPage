ec6612c5434080e5d118b3896aafda8a5ee82141
chenzhenfeng <czfgreat@gmail.com>
2014-11-17 17:09:08 +0800

    first commit

---
+++ src/com/example/urltest/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.urltest;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
